adfdc0eef4be3016e3915323d5102a111251c84b
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-06-24 17:00:56 -0300

    Add eslint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,14 +32,16 @@ export default function Home({ title = "React Avançado!" }: Props) {
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card">
+            className="card"
+          >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card">
+            className="card"
+          >
             <h3>Deploy &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
@@ -52,7 +54,8 @@ export default function Home({ title = "React Avançado!" }: Props) {
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
