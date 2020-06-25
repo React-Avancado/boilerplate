934d608cecab497d8bc31b74cdebdf1b9d8aa94d
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-06-25 18:41:32 -0300

    Add RTL config and first test

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+const Main = () => (
+  <main>
+    <h1>React Avançado</h1>
+  </main>
+)
+
+export default Main
---
+++ src/components/Main/test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Main />)
+
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
