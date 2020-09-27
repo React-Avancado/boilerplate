595e73af49bc0a6541b6155b3e2367a54cf6217c
Willian Justen <willianjustenqui@gmail.com>
2020-09-27 20:16:49 -0300

    Update Storybook to work absolute paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials']
+  addons: ['@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
