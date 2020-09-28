c78105794ca211104a861562e3493d1083f94b7f
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-09-28 15:34:09 -0300

    Update styled-components to 5.2 and add workaround to make tests to work with browser bundle

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,9 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/']
+  modulePaths: ['<rootDir>/src/'],
+  moduleNameMapper: {
+    '^styled-components':
+      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+  }
 }
