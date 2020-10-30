33aa95775ba0145e4bffc32b1562326b07b4c811
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-10-30 20:53:39 -0300

    Update styled-components to 5.2.1 to fix jest-dom matchers

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,5 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/'],
-  moduleNameMapper: {
-    '^styled-components':
-      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
-  }
+  modulePaths: ['<rootDir>/src/']
 }
