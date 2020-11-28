769fcbab7e3a05db705c40906fd5096c73eec745
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-11-28 15:18:02 -0300

    Update all packages and make storybook work with new optimizations and jsx

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
+  babel: async (options) => ({
+    ...options,
+    plugins: [...options.plugins, '@babel/plugin-transform-react-jsx']
+  }),
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
