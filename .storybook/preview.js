7b24da6789f9bed6c010791c45bcd2c3d38efd76
Paulo Silva <pauloalexandreduartesilva@gmail.com>
2020-11-28 17:54:42 +0000

    add storybook-addon-next-router to storybook decorators (#202)
    Co-authored-by: paulosilva <paulosilva@chinasystems.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
+import { addDecorator } from '@storybook/react'
+import { withNextRouter } from 'storybook-addon-next-router'
 import GlobalStyles from '../src/styles/global'
+
+addDecorator(withNextRouter())
 
 export const decorators = [
   (Story) => (
