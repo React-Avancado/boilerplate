fd6082f4bf9cccba3af27bb0f7d0418ca15d8c75
Flavio <xboard@users.noreply.github.com>
2021-03-15 21:52:39 -0300

    Add missing "theme-color" meta tag.
    In videos the `src/pages_app.tsx` file has an meta with name "theme-color".

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
