0305d8ec4834cb1724738dd9d167e2c7ee255e72
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-06-23 15:45:54 -0300

    Update versions

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,10 @@ module.exports = {
   addons: ['@storybook/addon-essentials'],
   babel: async (options) => ({
     ...options,
-    plugins: [...options.plugins,  require.resolve('@babel/plugin-transform-react-jsx')]
+    plugins: [
+      ...options.plugins,
+      require.resolve('@babel/plugin-transform-react-jsx')
+    ]
   }),
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
