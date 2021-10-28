1906fc898c345ad08048ac777237cf1500a59000
Luiz Tanure <letanure@gmail.com>
2021-10-28 13:36:41 -0300

    chore(next-upgrade): enable SWC as replacement for Babel
    warn  - Disabled SWC as replacement for Babel because of custom Babel configuration ".babelrc" https://nextjs.org/docs/messages/swc-disabled

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
+  swcMinify: true,
   pwa: {
     dest: 'public',
     disable: !isProd
