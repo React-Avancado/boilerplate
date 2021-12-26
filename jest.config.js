39a6b25eb7d64ea1ad9153fe123743b8c0bae38e
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-12-26 17:27:42 -0300

    Update to use SWC instead of Babel

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/']
+  modulePaths: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  }
 }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
+  swcMinify: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
   pwa: {
     dest: 'public',
     disable: !isProd
